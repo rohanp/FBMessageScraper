9f9e2b2fe9ac208334b18d0f3618edcd11e662c5
Rohan Pandit <rohanp@Rohans-MBP.home>
2015-06-28 21:32:30 -0400

    finished graphing methods!

--- grapher.py
+++ grapher.py
@@ -8,15 +8,18 @@ import os
 import datetime
 from dateutil.parser import parse
 import pickle as pkl
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as pyplot
 import matplotlib.dates as mdates
+from matplotlib import cm
 
 __author__ = "Rohan Pandit"
 __copyright__ = "Copyright {0}".format(datetime.date.today().year)
 
 pp = pprint.PrettyPrinter(indent=4)
 
-Message = namedtuple("Message", ['person', 'timestamp'])
+Message = namedtuple("Message", ['person', 'timestamp', 'content'])
 
 def main():
 	os.chdir("Messages")
@@ -27,12 +30,63 @@ def main():
 	messages += loadHangouts('2')
 	pkl.dump(messages, open("../messages.pkl", 'wb'))
 	"""
+
 	messages = pkl.load(open("../messages.pkl", 'rb'))
+	print("Total Messages: ", len(messages), "\n")
 
-	print("Total Messages: ", len(messages))
+	start_date = parse('7/26/14')
+	end_date = parse('6/20/15')
+	delta = (end_date - start_date).days
+	
+	people_msgs = {}
 
-	plotTotalMessages(messages, '7/25/14', '6/25/15')
+	for msg in messages:
+		if msg.person in people_msgs.keys():
+			if start_date < msg.timestamp < end_date:
+				idx = (msg.timestamp - start_date).days
+				people_msgs[msg.person][idx] += 1
+		else:
+			people_msgs[msg.person] = np.zeros(delta)
 
+	people_msgs = OrderedDict( sorted(people_msgs.items(), key=lambda i: -sum(i[1])) )
+
+	data = pd.DataFrame(people_msgs, index=pd.date_range(start_date, periods=delta))
+	top10 = data.iloc[:,:15]
+	print(top10.describe())
+	mean = data.mean().sum()
+	print("Mean: ", mean)	
+
+	#'#D44D5C'
+	ax = toPlot.plot(title="Cumulative Messaging Data (Top 10, #1 Excluded)", \
+		color=['#DDA561','#682E2E','#00ACC1','#CC3300','#FC5F20','#6EEB83', 'k', 'm','b' ])
+	ax.set_ylabel("Num Messages (Cumulative)")
+
+	pyplot.show()
+	plotTotalMessages(messages, start_date, end_date)
+
+def cumMsgPlot(data, x):
+	sum_data = data.apply(np.cumsum)
+	return sum_data.iloc[:,1:x]
+
+def totalMsgsTopXPlot(data, x): #bar graph
+	sum_data = data.iloc[:,:x].sum(axis=0)
+	return sum_data
+
+def numTalkedToPlot(data, min_messages=1, rolling=1):
+	talkedTo = data[ min_messages > data ]
+	talkedTo = pd.isnull(talkedTo)
+	toPlot = talkedTo.iloc[:,:].sum(axis=1)
+	return pd.rolling_mean(toPlot, rolling)
+
+def topXPlot(data, x, rolling=1):
+	others = data.iloc[:,:x].sum(axis=1)
+	top = data.iloc[:,x:]
+	top['Other'] = others
+	return pd.rolling_mean(top, rolling)
+
+def everyonePlot(data, rolling=1):
+	sum_data = data.iloc[:,:].sum(axis=1)
+	return pd.rolling_mean(sum_data, rolling)
 
 def loadMessages():
 	decoded_jsons = []
@@ -53,17 +107,30 @@ def loadMessages():
 	messages = []
 
 	for decoded_json in decoded_jsons:
-		try:
+
+		if str(decoded_json['other_user_fbid']) in people.keys():
 			person = getPerson(decoded_json['other_user_fbid'])
-		except KeyError:
-			person = decoded_json['other_user_fbid']
+		else:
+			person = str(decoded_json['other_user_fbid'])
+			if person == 'None':
+				person = decoded_json['author'][5:]
+				if str(person) in people.keys():
+					person = getPerson(person)
 		try:
 			timestamp = parse(decoded_json['timestamp_datetime'])
  			#TODO: Fix dates for last week 
 		except ValueError:
 			timestamp = parseDate(decoded_json['timestamp_datetime'])
+		try:
+			content = decoded_json['body']
+		except KeyError:
+			content = ""
 
-		messages.append(Message(person, timestamp))
+		if person == 'None':
+			pp.pprint(decoded_json)
+			quit()
+
+		messages.append(Message(person, timestamp, content))
 
 	return messages
 
@@ -79,11 +146,19 @@ def loadHangouts(a=''):
 			try:
 				person = getPerson(msg['conversation_id']['id'])
 			except KeyError:
-				person = msg['conversation_id']['id']
+				person = str(msg['conversation_id']['id'])
 
 			timestamp = datetime.datetime.fromtimestamp(int(msg['timestamp'])//1000000)
 
-			messages.append(Message(person, timestamp))
+			try:
+				content = msg['chat_message']['message_content']['segment'][0]['text']
+			except KeyError:
+				content = ""
+
+			if person == 'UgxG4T1spRtMbhsFK854AaABAQ':
+				print(content)
+
+			messages.append(Message(person, timestamp, content))
 
 	print("Total Hangouts: ", len(messages))
 	return messages
@@ -151,47 +226,61 @@ def parseDate(timestamp):
 
 
 def getPerson(_id):
-	return person[_id]
+	return people[str(_id)]
 
-person = {
-		100004255182066:'Ankit',
-		100001629407080:'Alison',
-		100000761719884:'Kevlin',
-		100000403221435:'Pranay',
-		100003238442927:'Vivek',
-		100004081027353:'Rayana',
-		100001128002655:'Michael You',
-		100004468539961:'Srijith',
-		100007730717911:'Philip Cho',
-		100004496133045:'Matthew Kaufer',
-		1702257415:'Andrew Huang',
-		100006199622129:'Anusha',
-		100005386399371:"Edward 'little shit' Shen",
-		1795540795:'Sashank',
-		100003272403235:'Tarun P.',
-		100002452600456:'Joseph',
-		100000756424022:'Jonathan',
-		100003296135651:'Cheryl',
-		100000471096235:'Josh Learn',
-		100000830703634:'Kaushik V.',
-		1497942558:'Avi',
-		100004831269618:'Tarun K.',
-		100003672744570:'Matthew S.',
-		100004537931899:'Rachel',
-		738046326264846:'Sheptiller',
-		1396634270650233:'Puzzle Hunt',
-		331888930354309:'Orgo',
-		836256343110704:'Dags',
-		943279359048126:'Dags',
-		484394795045364:'Dags',
-		1604705696418394:'Sheptiller',
-		711430812206486:'Church of Acio',
-		1417642561853941:'TJ Hunt',
-		550377098381585:'Derp City',
-		789573971100453:'Rachel',
+people = {
+		"100004255182066":'Ankit',
+		"100001629407080":'Alison',
+		"100000761719884":'Kevlin',
+		"100000403221435":'Pranay',
+		"100003238442927":'Vivek',
+		"100004081027353":'Rayana',
+		"100001128002655":'Michael You',
+		"100004468539961":'Srijith',
+		"100007730717911":'Philip Cho',
+		"100004496133045":'Matthew Kaufer',
+		"1702257415":'Andrew Huang',
+		"100006199622129":'Anusha',
+		"100005386399371":"Edward 'little shit' Shen",
+		"1795540795":'Sashank',
+		"100003272403235":'Tarun P.',
+		"100002452600456":'Joseph',
+		"100000756424022":'Jonathan',
+		"100003296135651":'Cheryl',
+		"100000471096235":'Josh Learn',
+		"100000830703634":'Kaushik V.',
+		"1497942558":'Avi',
+		"100004831269618":'Tarun K.',
+		"100003672744570":'Matthew S.',
+		"100004537931899":'Rachel',
+		"738046326264846":'Sheptiller',
+		"1396634270650233":'Puzzle Hunt',
+		"331888930354309":'Orgo',
+		"836256343110704":'Dags',
+		"943279359048126":'Dags',
+		"484394795045364":'Dags',
+		"1604705696418394":'Sheptiller',
+		"711430812206486":'Church of Acio',
+		"1417642561853941":'TJ Hunt',
+		"550377098381585":'Derp City',
+		"789573971100453":'Rachel',
+		"100004537931899":'Rachel',
+		"1795540795":'Sashank',
+		"100003746848349":'Rachel',
 		'UgxCAFWks7t61MHbACN4AaABAQ':'Dilip',
 		'Ugy9iByzX24wkgfWXFB4AaABAQ':'Rachel',
-		'Ugye5v649RFv0GFPx-B4AaABAQ':'Rachel'
+		'Ugye5v649RFv0GFPx-B4AaABAQ':'Rachel',
+		'UgxFVgsVFhQqij5yLUd4AaABAQ':'Pranay',
+		'UgwZ3G47YOszRHTUrat4AaABAQ':'Pranay',
+		'UgwY4fwEZHPvShvTFd94AaABAQ':'Kareem',
+		'UgzyTtDvfF4NRX6-NEd4AaABAQ':'Kevlin',
+		'100000756156415':'Ankitha',
+		'100001947235776':'Harsha',
+		'Ugy3WI1iNAVVyqtAz8J4AaABAQ':'Srijith',
+		'UgxG4T1spRtMbhsFK854AaABAQ':'Ellis',
+		'100003774101840':'Jay W.',
+		'100000452755853':'Ellis',
+		'100003791942334':'Pranay Dubey'
 		}
 
 if __name__ == "__main__": main()
